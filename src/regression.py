c8be030d2fd99558b8d9897d5b44a54c0b877cbe
raunak <0506cs231111@gmail.com>
2025-12-17 17:00:37 +0530

    created linearregression file and also implemented LR with GD

---
+++ src/regression.py
@@ -0,0 +1,43 @@
+import numpy as np 
+
+# Linear Regression using OLS 
+class linearRegression :
+
+    def __init__(self):
+        self.coef_ = None 
+        self.intercept_ = None 
+        
+        
+    def fit (self , X , y ):
+        X = np.insert(X , 0 , 1 , axis = 1 )
+
+        weights = np.linalg.inv((X.T@X))@(np.transpose(X))@y
+        self.intercept_ = weights[0]
+        self.coef_  = weights[1:]
+
+    def predict(self, X):
+        return np.dot(X,self.coef_) + self.intercept_
+        
+
+class LinearRegressionGD:
+
+    def __init__(self, lr=0.01, epoches=50):
+        self.lr = lr
+        self.epoches = epoches
+        self.coef_ = None
+        self.intercept_ = None
+
+    def fit(self, X, y):
+        X = np.insert(X, 0, 1, axis=1)
+        weights = np.zeros(X.shape[1])
+
+        for i in range(self.epoches):
+            y_pred = X @ weights
+            gradient = (X.T @ (y - y_pred)) / X.shape[0]
+            weights = weights + self.lr * gradient
+
+        self.intercept_ = weights[0]
+        self.coef_ = weights[1:]
+
+    def predict(self, X):
+        return X @ self.coef_ + self.intercept_
